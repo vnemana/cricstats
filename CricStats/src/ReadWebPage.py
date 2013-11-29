b4ef5323c02d2566308f1f7c1e10baa79c1e593a
Venkat Nemana <venkat.nemana.code@gmail.com>
2013-11-28 18:10:16 -0800

    Initial Version
    Change-Id: Ib24b5ec8261028946ca0376bf3c123bc6e1c1151
    Signed-off-by: Venkat Nemana <venkat.nemana.code@gmail.com>

---
+++ CricStats/src/ReadWebPage.py
@@ -0,0 +1,22 @@
+import urllib2
+from HTMLParser import HTMLParser
+
+response = urllib2.urlopen ('http://www.espncricinfo.com/new-zealand-v-west-indies-2013-14/engine/match/683779.html?wrappertype=print');
+html = response.read();
+
+class myHtmlParser (HTMLParser):
+    is_table = False;
+    def handle_starttag(self, s_tag, attrs):
+        if (s_tag == 'table'):
+            self.is_table = True;
+        print "Encountered a start tag:", s_tag
+    def handle_data(self, data):
+        if (self.is_table == True):
+            print "Encountered some data  :", data
+    def handle_endtag(self, tag):
+        if (self.is_table == True):
+            print "Encountered an end tag :", tag
+            self.is_table = False;
+
+parser = myHtmlParser()
+parser.feed(html)
