2d985f0fdca0d5ae441a2f7c3a7e93135f4f6a3d
Venkat Nemana <venkat.nemana.code@gmail.com>
2014-06-28 19:13:58 -0700

    Comitting from Studio

--- CricStats/src/ReadWebPage.py
+++ CricStats/src/ReadWebPage.py
@@ -1,22 +1,26 @@
 import urllib2
 from HTMLParser import HTMLParser
 
-response = urllib2.urlopen ('http://www.espncricinfo.com/new-zealand-v-west-indies-2013-14/engine/match/683779.html?wrappertype=print');
-html = response.read();
+response = urllib2.urlopen('file:///Users/venkatakrishnanemana/Code/cricstats/SampleScoresheet.html')
+html = response.read()
 
-class myHtmlParser (HTMLParser):
-    is_table = False;
+
+class MyHtmlParser (HTMLParser):
+    is_table = False
+
     def handle_starttag(self, s_tag, attrs):
-        if (s_tag == 'table'):
-            self.is_table = True;
+        if s_tag == 'table':
+            self.is_table = True
         print "Encountered a start tag:", s_tag
-    def handle_data(self, data):
-        if (self.is_table == True):
-            print "Encountered some data  :", data
-    def handle_endtag(self, tag):
-        if (self.is_table == True):
-            print "Encountered an end tag :", tag
-            self.is_table = False;
 
-parser = myHtmlParser()
+    def handle_data(self, data):
+        if self.is_table is True:
+            print "Encountered some data  :", data
+
+    def handle_endtag(self, tag):
+        if self.is_table is True:
+            print "Encountered an end tag :", tag
+            self.is_table = False
+
+parser = MyHtmlParser()
 parser.feed(html)
